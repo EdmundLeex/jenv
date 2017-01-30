9177967dfb6520240f5d7591e2643383815509de
Edmund Li <edmund.xz.lee@gmail.com>
2017-01-29 16:47:31 -0800

    fix cli command

--- bin/init.js
+++ bin/init.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 const shell = require('shelljs');
+const app = require('commander');
 
-shell.exec('mkdir config');
-shell.exec('cp lib/env_template.json config/env.json');
+app.command('init').action(function () {
+  shell.exec('mkdir config');
+  shell.exec('cp lib/env_template.json config/env.json');
+});
