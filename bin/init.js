4918fd332908ed80b179add74a8c00b9ba974575
Edmund Li <edmund.xz.lee@gmail.com>
2017-01-29 17:23:07 -0800

    fixed route

--- bin/init.js
+++ bin/init.js
@@ -6,7 +6,7 @@ const command = process.argv.slice(2)[0];
 
 if (command === 'init') {
   shell.exec('mkdir config');
-  shell.exec('cp lib/env_template.json config/env.json');
+  shell.exec('cp node_modules/jenv/lib/env_template.json config/env.json');
   console.log('config/env.json');
 } else {
   console.error(`${command} is an invalid command`);
