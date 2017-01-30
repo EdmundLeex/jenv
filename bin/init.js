91c42bb9fff2cad3a2758b1d79b4d76b541c8e38
Edmund Li <edmund.xz.lee@gmail.com>
2017-01-29 17:21:07 -0800

    actually fix init script

--- bin/init.js
+++ bin/init.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
 const shell = require('shelljs');
-const app = require('commander');
 
-app.command('init').action(function () {
+const command = process.argv.slice(2)[0];
+
+if (command === 'init') {
   shell.exec('mkdir config');
   shell.exec('cp lib/env_template.json config/env.json');
-});
+  console.log('config/env.json');
+} else {
+  console.error(`${command} is an invalid command`);
+}
